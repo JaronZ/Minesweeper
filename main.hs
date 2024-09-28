0e378d6c938f9475eeae4dca1f90e48e8b335b83
Jaron Zijlstra <jaron_zijlstra@outlook.com>
2024-09-28 15:28:35 +0200

    feat: make game repeatable

--- main.hs
+++ main.hs
@@ -1,4 +1,15 @@
 import Game (minesweeper)
+import Data.Char (toUpper)
 
 main :: IO ()
-main = minesweeper
+main = do minesweeper
+          putStr "Do you want to play again? (Y/N): "
+          {-
+          getChar is not used here because of a bug in Haskell on Windows.
+          using getChar would make it so the game can only be played twice.
+          -}
+          playAgain <- getLine
+          if toUpper (head playAgain) == 'Y' then
+            main
+          else
+            return ()
